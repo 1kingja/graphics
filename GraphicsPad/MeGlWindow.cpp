435d262c5177a63316aab41a8eebc00d34cdb8fa
Jamie King <Jamie King>
2015-12-17 14:04:20 -0500

    Video 4 done. OpenGL Buffer Data. Sets up a simple triangle vertice data on the stack, creates OpenGL buffers to put the data in, and sends the data down to the buffer

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -4,10 +4,20 @@
 void MeGlWindow::initializeGL()
 {
 	glewInit();
+
+	GLfloat verts[] =
+	{
+		+0.0f, +1.0f,
+		-1.0f, -1.0f,
+		+1.0f, -1.0f,
+	};
+	GLuint myBufferID;
+	glGenBuffers(1, &myBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
+		verts, GL_STATIC_DRAW);
 }
 
 void MeGlWindow::paintGL()
 {
-	glClearColor(1, 0, 0, 1);
-	glClear(GL_COLOR_BUFFER_BIT);
 }
