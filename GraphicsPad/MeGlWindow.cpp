b4c31c76e5a1b55652029f22cbad1e9e6d30152a
Jamie King <Jamie King>
2015-12-17 15:59:29 -0500

    Video 14 done. Vertex Attributes Triangle Interpolation. Changes the vertex colors to red, green, and blue and discusses blending/interpolation of the color values between the vertices.

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -8,16 +8,12 @@ void sendDataToOpenGL()
 {
 	GLfloat verts[] =
 	{
-		+0.0f, +0.0f,
-		+1.0f, +0.0f, +0.0f,
-		+1.0f, +1.0f,
-		+1.0f, +0.0f, +0.0f,
-		-1.0f, +1.0f,
+		+0.0f, +1.0f,
 		+1.0f, +0.0f, +0.0f,
 		-1.0f, -1.0f,
-		+1.0f, +0.0f, +0.0f,
+		+0.0f, +1.0f, +0.0f,
 		+1.0f, -1.0f,
-		+1.0f, +0.0f, +0.0f,
+		+0.0f, +0.0f, +1.0f,
 	};
 	GLuint myBufferID;
 	glGenBuffers(1, &myBufferID);
@@ -29,7 +25,7 @@ void sendDataToOpenGL()
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
 
-	GLushort indices[] = { 0,1,2, 0,3,4 };
+	GLushort indices[] = { 0,1,2 };
 	GLuint indexBufferID;
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
