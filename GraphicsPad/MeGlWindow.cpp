fe98df9c527565d4217ce528f041e95f41c531cb
Jamie King <Jamie King>
2015-12-17 15:04:59 -0500

    Video 9 done. OpenGL Vertex Colors. Adds color data to the verts. Doesn't render color, waits for future video where we setup shaders.

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -8,10 +8,15 @@ void MeGlWindow::initializeGL()
 	GLfloat verts[] =
 	{
 		+0.0f, +0.0f,
+		+0.0f, +0.0f, +0.0f,
 		+1.0f, +1.0f,
+		+0.0f, +0.0f, +0.0f,
 		-1.0f, +1.0f,
+		+0.0f, +0.0f, +0.0f,
 		-1.0f, -1.0f,
+		+0.0f, +0.0f, +0.0f,
 		+1.0f, -1.0f,
+		+0.0f, +0.0f, +0.0f,
 	};
 	GLuint myBufferID;
 	glGenBuffers(1, &myBufferID);
@@ -19,9 +24,11 @@ void MeGlWindow::initializeGL()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
 		verts, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
 
-	GLushort indices[] = { 3,1,2, 0,3,4 };
+	GLushort indices[] = { 0,1,2, 0,3,4 };
 	GLuint indexBufferID;
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
