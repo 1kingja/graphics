3b8422f17fce45bac54fff04189f5166263be179
Jamie King <Jamie King>
2015-12-17 22:00:17 -0500

    Video 21 done. OpenGL glEnable Depth Test. Adds a z coordinate to the vertex data and adjusts the attribs and shader values accordingly. Has red and blue triangle compete for nearest depth.

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -39,20 +39,22 @@ bool checkProgramStatus(GLuint programID)
 
 void sendDataToOpenGL()
 {
+	const float RED_TRIANGLE_Z = 0.5;
+	const float BLUE_TRIANGLE_Z = -0.5;
 	GLfloat verts[] =
 	{
-		-1.0f, -1.0f,
+		-1.0f, -1.0f, RED_TRIANGLE_Z,
 		+1.0f, +0.0f, +0.0f,
-		+0.0f, +1.0f,
+		+0.0f, +1.0f, RED_TRIANGLE_Z,
 		+1.0f, +0.0f, +0.0f,
-		+1.0f, -1.0f,
+		+1.0f, -1.0f, RED_TRIANGLE_Z,
 		+1.0f, +0.0f, +0.0f,
 
-		-1.0f, +1.0f,
+		-1.0f, +1.0f, BLUE_TRIANGLE_Z,
 		+0.0f, +0.0f, +1.0f,
-		+0.0f, -1.0f,
+		+0.0f, -1.0f, BLUE_TRIANGLE_Z,
 		+0.0f, +0.0f, +1.0f,
-		+1.0f, +1.0f,
+		+1.0f, +1.0f, BLUE_TRIANGLE_Z,
 		+0.0f, +0.0f, +1.0f,
 	};
 	GLuint myBufferID;
@@ -61,11 +63,11 @@ void sendDataToOpenGL()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
 		verts, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3));
 
-	GLushort indices[] = { 3,4,5, 0,1,2,  };
+	GLushort indices[] = { 0,1,2, 3,4,5, };
 	GLuint indexBufferID;
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
@@ -119,13 +121,14 @@ void installShaders()
 void MeGlWindow::initializeGL()
 {
 	glewInit();
+	glEnable(GL_DEPTH_TEST);
 	sendDataToOpenGL();
 	installShaders();
 }
 
 void MeGlWindow::paintGL()
 {
+	glClear(GL_DEPTH_BUFFER_BIT);
 	glViewport(0, 0, width(), height());
-	//glDrawArrays(GL_TRIANGLES, 0, 6);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 }
