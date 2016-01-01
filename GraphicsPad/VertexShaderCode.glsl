6d31445a3e3f7b7fd50e802b884ebb824378ff68
Jamie King <Jamie King>
2016-01-01 11:33:36 -0700

    Video 93 done. Relied on OpenGL to fill in fourth comonent of our position vector with a 1.

--- GraphicsPad/VertexShaderCode.glsl
+++ GraphicsPad/VertexShaderCode.glsl
@@ -1,6 +1,6 @@
 #version 430
 
-in layout(location=0) vec3 vertexPositionModel;
+in layout(location=0) vec4 vertexPositionModel;
 in layout(location=1) vec3 vertexColor;
 in layout(location=2) vec3 normalModel;
 uniform vec3 ambientLight;
@@ -13,8 +13,7 @@ out vec3 vertexPositionWorld;
 
 void main()
 {
-	vec4 v = vec4(vertexPositionModel, 1.0);
-	gl_Position = modelToProjectionMatrix * v;
+	gl_Position = modelToProjectionMatrix * vertexPositionModel;
 	normalWorld = vec3(modelToWorldMatrix * vec4(normalModel, 0));
-	vertexPositionWorld = vec3(modelToWorldMatrix * v);
+	vertexPositionWorld = vec3(modelToWorldMatrix * vertexPositionModel);
 }
